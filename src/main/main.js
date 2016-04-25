109330491075e997089bce3aeed2eeb9835b78f2
Vadim Kulik <xbyte.com.ua@gmail.com>
2016-04-25 18:00:31 +0300

    return deletet files

---
+++ src/main/main.js
@@ -0,0 +1,5 @@
+setTimeout(function(){
+ 	$.msgGrowl ({type: "success",lifetime:"2500", title: "Hi Friend", text: "I'm happy to see you"})},500);
+ 
+ setTimeout(function(){
+ 	$.msgGrowl ({type: "success",lifetime:"2500", title: "Hey", text: "Realy!!! Lets drinknem"})},4000);
